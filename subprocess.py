13e213f1e68665c7b32efbdcb56179eb0e3524a6
xtenex <tenex.whatever@gmail.com>
2020-08-09 16:08:33 -0700

    Update subprocess.py

--- subprocess.py
+++ subprocess.py
@@ -11,6 +11,8 @@ def main():
     #print("""write your commnad with or without arguments like "ls -alh"\n""")
     try:
         subprocess.Popen(subprocess.sys.argv[1:])
+        # Aqui hay algo interesante, si quitamos los corchetes de argv dejandolo 'subprocess.sys.argv" tal cual, entra en un ciclo infinito
+        # something fun is: if you leave just argv withouth brackets, the program falls into an infinite loop
         exit(0)
     except IndexError:
         print("Faltan argumentos: {subprocess.sys.argv[0]} [comando como lo escribiria en linea de comandos]")
